29726cf82e8711ddf88171315f4e6f688f35a762
6democratickim9 <shcomon24@gmail.com>
2021-08-09 22:34:10 +0900

    0809

---
+++ 08-Overriding-Polymorphism-inst/src/step7/Manager.java
@@ -0,0 +1,14 @@
+package step7;
+
+public class Manager extends Employee{
+	private String department;
+
+	public Manager(String empNo, String name, int salary, String department) {
+		super(empNo, name, salary);
+		this.department = department;
+	}
+	@Override
+	public String toString() {	
+		return super.toString()+" department:"+department;
+	}
+}
