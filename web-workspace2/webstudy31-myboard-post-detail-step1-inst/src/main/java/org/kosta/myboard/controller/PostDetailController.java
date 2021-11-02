0e2bc0d404c7a9fed4408d0453b00fd593253c13
Bittah Swami <41178536+6democratickim9@users.noreply.github.com>
2021-11-02 09:44:03 +0900

    1102

---
+++ web-workspace2/webstudy31-myboard-post-detail-step1-inst/src/main/java/org/kosta/myboard/controller/PostDetailController.java
@@ -0,0 +1,23 @@
+package org.kosta.myboard.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.kosta.myboard.model.BoardDAO;
+import org.kosta.myboard.model.PostVO;
+
+public class PostDetailController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String no=request.getParameter("no");
+		//조회수 증가 
+		BoardDAO.getInstance().updateHits(no);
+		//상세 게시물 조회 
+		PostVO pvo=BoardDAO.getInstance().getPostingByNo(no);
+		request.setAttribute("pvo", pvo);
+		request.setAttribute("url", "board/post-detail.jsp");
+		return "layout.jsp";
+	}
+
+}
