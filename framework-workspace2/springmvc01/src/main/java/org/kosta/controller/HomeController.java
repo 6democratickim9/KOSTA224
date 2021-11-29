1b70336a15d9d73e2947534d2d68054db783d9f1
Bittah Swami <41178536+6democratickim9@users.noreply.github.com>
2021-11-29 23:43:41 +0900

    1129

--- framework-workspace2/springmvc01/src/main/java/org/kosta/controller/HomeController.java
+++ framework-workspace2/springmvc01/src/main/java/org/kosta/controller/HomeController.java
@@ -16,7 +16,7 @@ public class HomeController {
 	@RequestMapping("home.do")
 	public String home(Model model) {
 		//request.setAttribute와 동일한 표현 
-		model.addAttribute("message", "Hello SpringMVC 즐거운 한주~");//model 정보 
+		model.addAttribute("message", "Hello SpringMVC 졸려용~");//model 정보 
 		// springmvc-servlet.xml의 viewResolver 설정에 의해
 		// 리턴하는 view name은 /WEB-INF/views/index.jsp 로 찾아서 응답하게 된다
 		return "index";//view name 
